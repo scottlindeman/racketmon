a2bf893aefb2f1ac7c9c00b8403d42d52f0737d5
scottlindeman <scott.lindeman@gmail.com>
2013-12-07 01:38:06 -0500

    Added structs for each type of resource

--- main.rkt
+++ main.rkt
@@ -1,11 +1,17 @@
 #lang racket
 
 (require net/url json)
+(require "models.rkt")
+
+(provide get)
 
 ; http://www.pokeapi.co/api/v1/
 ;
 ; pokemon/NUMPOKEMON
 ; Where NUMPOKEMON is a Num and 1 <= NUMPOKEMON <= 718
+; -OR-
+; pokemon/NAMEPOKEMON
+; Where NAMEPOKEMON is the name of a pokemon
 ;
 ; ability/NUMABILITY
 ; Where NUMABILITY is a Num and 1 <= NUMBAILITY <= 248
@@ -26,6 +32,13 @@
 ; - NUMMOVE
 ; - NUMEGG
 
+; A Resource is one of:
+; - 'pokemon
+; - 'ability
+; - 'type
+; - 'move
+; - 'egg
+
 (define BASE-URL "http://www.pokeapi.co/api/v1/")
 
 ;; An APIURI is one of:
@@ -34,6 +47,10 @@
 (define TYPE-URL "type/")
 (define MOVE-URL "move/")
 (define EGG-URL  "egg/")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; URL CREATION
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; APIURI POKENUM -> URL
 ;; Creates a URL for the specified api call
@@ -60,18 +77,88 @@
 ;; Creates a URL structure for the given NUMEGG
 (define (egg-url num) (create-api-url EGG-URL num))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; MAKING REQUESTS
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;; input-port -> JsExpr | #f
 ;; Given a port, if the port can be read into a json then the json is returned
-(define (poke-port port)
+(define (port-handler port)
   (let ([data (read-json port)])
     (if (jsexpr? data)
         data
         #f)))
 
-;; NUMPOKEMON -> JsExpr
-;; Retrieves the JSON data from the rest call for pokemon data
-(define (call/pokemon num)
-  (let ([url (pokemon-url num)])
+;; [POKENUM -> JsExpr] POKENUM -> JsExpr
+;; Applies the given url function
+(define (make-call url-fn num)
+  (let ([url (url-fn num)])
     (call/input-url url
                     get-pure-port
-                    poke-port)))
+                    port-handler)))
+
+;; NUMPOKEMON -> JsExpr
+;; Retrieves the JSON data from the rest call for pokemon data
+(define (call/pokemon num) (make-call pokemon-url num))
+
+;; NUMABILITY -> JsExpr
+;; Retrieves the JSON data from the rest call for ability data
+(define (call/ability num) (make-call ability-url num))
+
+;; NUMTYPE -> JsExpr
+;; Retrieves the JSON data from the rest call for type data
+(define (call/type num) (make-call type-url num))
+
+;; NUMEGG -> JsExpr
+;; Retrieves the JSON data from the rest call for egg data
+(define (call/egg num) (make-call egg-url num))
+
+(define (get resource-type #:id [id #f] #:name [name #f])
+  (define rid (cond [id id]
+                    [name name]
+                    [else 0]))
+  (cond [(symbol=? resource-type 'pokemon)
+         (create-pokemon (call/pokemon rid))]
+        [else void]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; CONVERSION FUNCTIONS
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (jsexpr-name h)
+  (hash-ref h 'name))
+
+(define (create-pokemon jsexpr)
+  (pokemon (hash-ref jsexpr 'national_id)
+           (hash-ref jsexpr 'pkdx_id)
+           (hash-ref jsexpr 'name)
+           (hash-ref jsexpr 'modified)
+           (hash-ref jsexpr 'created)
+           (hash-ref jsexpr 'exp)
+           (hash-ref jsexpr 'height)
+           (map jsexpr-name (hash-ref jsexpr 'types))
+           (map jsexpr-name (hash-ref jsexpr 'abilities))
+           (hash-ref jsexpr 'attack)
+           (hash-ref jsexpr 'defense)
+           (hash-ref jsexpr 'sp_atk)
+           (hash-ref jsexpr 'sp_def)
+           (hash-ref jsexpr 'catch_rate)
+           (hash-ref jsexpr 'egg_cycles)
+           (map jsexpr-name (hash-ref jsexpr 'egg_groups))
+           (hash-ref jsexpr 'ev_yield)
+           (create-evolution (hash-ref jsexpr 'evolutions))
+           (hash-ref jsexpr 'growth_rate)
+           (hash-ref jsexpr 'happiness)
+           (hash-ref jsexpr 'hp)
+           (hash-ref jsexpr 'male_female_ratio)
+           (map jsexpr-name (hash-ref jsexpr 'moves))
+           (hash-ref jsexpr 'species)
+           (hash-ref jsexpr 'speed)
+           (hash-ref jsexpr 'sprites) ;; Not sure what to do for sprites
+           (hash-ref jsexpr 'total)
+           (hash-ref jsexpr 'weight)))
+
+(define (create-evolution jsexpr)
+  (evolution (hash-ref jsexpr 'method)
+             (hash-ref jsexpr 'level)
+             (hash-ref jsexpr 'to)))
---
+++ models.rkt
@@ -0,0 +1,130 @@
+#lang racket
+
+(provide (struct-out pokemon)
+         (struct-out evolution))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  RESOURCES
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; The follow structure definitions represent the resources available to get from
+; the pokeapi API. They are as follows:
+; - Pokemon
+; - Move
+; - Ability
+; - Type
+; - Egg
+; - Pokedex
+; - Sprite
+; - Game
+; - Description
+
+;; A Pokemon is a (pokemon arg arg ...)
+;; Represents a pokemon
+(define-struct pokemon (national-id        ;; Num
+                        pokedex-id         ;; Num
+                        name               ;; String
+                        modified           ;; Date
+                        created            ;; Date
+                        exp                ;; Num
+                        height             ;; String
+                        types              ;; [Listof String]
+                        abilities          ;; [Listof String]
+                        attack             ;; Num
+                        defense            ;; Num
+                        sp-attack          ;; Num
+                        sp-defense         ;; Num
+                        catch-rate         ;; Num
+                        egg-cycles         ;; Num
+                        egg-groups         ;; [Listof String]
+                        ev-yield           ;; String
+                        evolutions         ;; [Listof Evloution]
+                        growth-rate        ;; String
+                        happiness          ;; Num
+                        hp                 ;; Num
+                        male-female-ratio  ;; String
+                        moves              ;; [Listof String]
+                        species            ;; String
+                        speed              ;; Num
+                        sprites            ;; [Listof ???]
+                        total              ;; Num
+                        weight))           ;; String
+
+;; A Move is a (move Num String Date Date Num String String Num Num)
+;; Represents a pokemon move
+(define-struct move (id
+                     name
+                     modified
+                     created
+                     accuracy
+                     category
+                     description
+                     power
+                     pp))
+
+;; An Ability is a (ability Num String Date Date String)
+;; Represents a pokemon ability
+(define-struct ability (id
+                        name
+                        modified
+                        created
+                        description))
+
+;; A Type is a (type Num String Date Date
+;;                   [Listof String] [Listof String] [Listof String] [Listof String])
+;; Represents a pokemon type
+(define-struct type (id
+                     name
+                     modified
+                     created
+                     ineffective
+                     no-effect
+                     super-effective
+                     weakness))
+
+;; An Egg is a (egg Num String Date Date [Listof String])
+;; Represents a pokemon egg group
+(define-struct egg (id
+                    name
+                    modified
+                    created
+                    pokemon))
+
+;; A Pokedex is a (Pokedex String Date Date [Listof String])
+;; Represents the pokedex, listing of all pokemon
+(define-struct pokedex (name
+                        modified
+                        created
+                        pokemon))
+
+;; A Sprite is a (sprite Num String Date Date String)
+;; Contains the uri to the image for the pokemon
+(define-struct sprite (id
+                       name
+                       modified
+                       pokemon
+                       image))
+
+;; A Game is a (game Num String Date Date)
+;; Represents a pokemon game
+(define-struct game (id
+                     name
+                     modified
+                     created))
+
+;; A Description is a (description Num String Date Date [Listof String] String)
+;; Represents a pokemon description
+(define-struct description (id
+                            name
+                            modified
+                            created
+                            games
+                            pokemon))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; NON-RESOURCES
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; An Evolution is a (evolution String Num String)
+;; Represents the evolution of a pokemon
+(define-struct evolution (method level to))
