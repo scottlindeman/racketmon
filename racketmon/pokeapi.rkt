e136839201a7ef660956e4b74646e50ccdbdf8d5
scottlindeman <scott.lindeman@gmail.com>
2013-12-20 13:16:37 -0600

    Restructing to fit racket package model

---
+++ racketmon/pokeapi.rkt
@@ -0,0 +1,5 @@
+#lang racket
+
+(require "private/model.rkt" "private/request.rkt" "private/model-util.rkt")
+
+(provide (all-from-out "private/model.rkt" "private/request.rkt" "private/model-util.rkt"))
